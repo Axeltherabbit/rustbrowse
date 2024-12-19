6888da6715d7015f012cb73077f6b9e9f0a46446
AxelTheRabbit <pietroconvalle@python.it>
2024-12-19 14:52:59 +0000

    moved parser code in its own folder

--- rustbrowse/src/main.rs
+++ rustbrowse/src/main.rs
@@ -1,24 +1,12 @@
-
-use url::{Url};
 use std::env;
 use std::collections::HashMap;
 
-mod socket; // This line is necessary to include the socket module
+mod socket; 
 use socket::socket::Socket;
 
+mod parser;
+use parser::parser::{Parser, HttpStatus, Url};
 
-fn parse_url(s_url: &str) -> Url {
-    let url = Url::parse(s_url).unwrap();
-    
-    let scheme = url.scheme();
-    if scheme != "http" {
-        panic!("Error, only http supported");
-    }
-    
-    url.host_str().unwrap();
-
-    return url;
-}
 
 fn build_header(url: &Url) -> String {
 
@@ -29,42 +17,6 @@ fn build_header(url: &Url) -> String {
     return [request_path_protocol, request_host, request_end].join("\r\n");
 }
 
-struct HttpStatus{
-    version: String,
-    status: String,
-    explanation: String
-}
-impl Default for HttpStatus {
-    fn default () -> HttpStatus {
-        HttpStatus {status:String::new(), version:String::new(), explanation:String::new()}
-    }
-}
-
-fn parse_response(response: &str, status: &mut HttpStatus, headers: &mut HashMap<String, String>, body: &mut String){
-    let mut lines = response.lines();
-    let mut statusline = lines.next().unwrap().splitn(3, " ");
-
-    status.version = String::from(statusline.next().unwrap());
-    status.status = String::from(statusline.next().unwrap());
-    status.explanation = String::from(statusline.next().unwrap());
-
-    let mut headerline = lines.next().unwrap();
-    while headerline != "" {
-        let mut keyval = headerline.splitn(2, ":");
-        // println!("{}", headerline);
-        headers.insert(
-             keyval.next().unwrap().to_lowercase(), 
-             String::from(keyval.next().unwrap().trim()));
-
-        headerline = lines.next().unwrap();
-    }
-
-    assert!(!headers.contains_key("transfer-encoding"));
-    assert!(!headers.contains_key("content-encoding"));
-    
-    body.push_str(lines.fold(String::new(), |a, b| a + b + "\n").as_str());
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     
@@ -73,7 +25,7 @@ fn main() {
         return;
     }
 
-    let url: Url = parse_url(args[1].as_str());
+    let url: Url = Parser::parse_url(args[1].as_str());
 
     let mut sock = Socket::new(String::from(url.host_str().unwrap()),
                            url.port_or_known_default().unwrap());
@@ -82,10 +34,11 @@ fn main() {
 
     let response = sock.read_response();
 
-    println!("{}", response);
-  
     let mut headers = HashMap::new();
     let mut body = String::new();
     let mut status = HttpStatus::default();
-    parse_response(response.as_str(), &mut status, &mut headers, &mut body);
+    Parser::parse_response(response.as_str(), &mut status, &mut headers, &mut body);
+
+    println!("{}", body);
+
 }
---
+++ rustbrowse/src/parser/parser.rs
@@ -0,0 +1,56 @@
+use std::collections::HashMap;
+pub use url::Url;
+
+pub struct HttpStatus{
+    version: String,
+    status: String,
+    explanation: String
+}
+impl Default for HttpStatus {
+    fn default () -> HttpStatus {
+        HttpStatus {status:String::new(), version:String::new(), explanation:String::new()}
+    }
+}
+
+
+pub struct Parser {}
+
+impl Parser{
+    pub fn parse_response(response: &str, status: &mut HttpStatus, headers: &mut HashMap<String, String>, body: &mut String){
+        let mut lines = response.lines();
+        let mut statusline = lines.next().unwrap().splitn(3, " ");
+
+        status.version = String::from(statusline.next().unwrap());
+        status.status = String::from(statusline.next().unwrap());
+        status.explanation = String::from(statusline.next().unwrap());
+
+        let mut headerline = lines.next().unwrap();
+        while headerline != "" {
+            let mut keyval = headerline.splitn(2, ":");
+            // println!("{}", headerline);
+            headers.insert(
+                 keyval.next().unwrap().to_lowercase(), 
+                 String::from(keyval.next().unwrap().trim()));
+
+            headerline = lines.next().unwrap();
+        }
+
+        assert!(!headers.contains_key("transfer-encoding"));
+        assert!(!headers.contains_key("content-encoding"));
+        
+        body.push_str(lines.fold(String::new(), |a, b| a + b + "\n").as_str());
+    }
+
+    pub fn parse_url(s_url: &str) -> Url {
+        let url = Url::parse(s_url).unwrap();
+        
+        let scheme = url.scheme();
+        if scheme != "http" {
+            panic!("Error, only http supported");
+        }
+        
+        url.host_str().unwrap();
+
+        return url;
+    }
+}
